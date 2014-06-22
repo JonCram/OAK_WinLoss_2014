ed187521654101125e4f78231edb8673e6cdfaea
cramcode <cramcode@gmail.com>
2014-06-22 15:42:41 -0600

    final formatting; ready for export

--- OAK_WinLoss_2014.R
+++ OAK_WinLoss_2014.R
@@ -7,15 +7,19 @@ df <- read.csv("~/_dev/analysis/OAK_WinLoss_2014/OAK_WinLoss_2014.csv")
 ggplot() + 
   geom_bar(data = df, aes(x=Gm, y=R)
            , stat = "identity"
-           , fill="#005596", width=.8) +
+           , fill="#00483A", width=.8) +
   geom_bar(data = df, aes(x=Gm, y=RA)
            , stat = "identity"
-           , fill="#A31410", width=.8) +
+           , fill="#FFBE00", width=.8) +
   xlab("Games") +
   ylab("Runs") +
   ggtitle("Athletics 2014: Runs Scored and Allowed") +
-  # annotate("text", x = 0, y = 8, label = "Some text") +
+  annotate("text", x = 4, y = 11, size=6
+           , label = "Runs Scored", colour = "#00483A") +
+  annotate("text", x = 4, y = -14, size=6
+           , label = "Runs Allowed", colour = "#FFBE00") +
+ 
   theme_cram() +
   theme(
-    # axis.text.x = element_text(angle=90,hjust=.5,vjust=.5)
+    axis.ticks.margin=unit(c(0,0),'cm')
   )
